2eb1e03e921996e56634778f0efbeb5c6a9a258b
SonicReal <alisher.nuritdinov@payme.uz>
2019-09-24 17:57:56 +0500

    update: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = undefined;
+        this.max = undefined;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
 
+        const result = Math.ceil((this.max + this.min) / 2);
+
+        return result;
     }
 
     lower() {
-
+        this.max = Math.ceil((this.max + this.min) / 2);
     }
 
     greater() {
-
+        const new_min = Math.ceil((this.max + this.min) / 2);
+        this.min = new_min
     }
 }
 
